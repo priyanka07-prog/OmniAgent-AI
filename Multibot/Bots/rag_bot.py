952affccc8c0ac799c4185875f0361ea02b69b37
priyankashani861@gmail.com <priyankasahni861@gmail.com>
2026-03-19 15:44:34 +0530

    Update file

--- Multibot/Bots/rag_bot.py
+++ Multibot/Bots/rag_bot.py
@@ -1,10 +1,10 @@
-from database.vectore_store import create_vector_store
+from database.vector_store import create_vector_store
 
 def rag_bot(question, documents):
     
-    vectorestore = create_vector_store(documents)
+    vector_store = create_vector_store(documents)
     
-    docs = vectorestore.similarity_search(question)
+    docs = vector_store.similarity_search(question)
     
     return docs[0].page_content
     
